2ce68c0db4fbc87dc90c0df19da3a120b7b94dd1
Sayyed Amaan Ali <122671745+Sayyed-Amaan-Ali@users.noreply.github.com>
2024-08-15 03:30:45 +0530

    Create tenant.ts

---
+++ Frontend/src/app/tenants/tenant.ts
@@ -0,0 +1,38 @@
+export interface Tenant{
+    id: number, 
+
+    name: string,
+    
+    email: string,
+    
+    phone: string,
+
+    address: string,
+    
+    profession: string,
+    
+    officename: string,
+    
+    officeaddress: string,
+
+    leasestartdate: Date,
+
+    rentduedate: Date,
+
+    paymentstatus: string,
+
+    rentamount: number,
+    
+    password: string,
+
+    tenantid: string,
+
+    roomno: string,
+    
+    roomtype: string,
+    
+    facility: string,
+    
+    vehicleinformation: string
+    
+}
